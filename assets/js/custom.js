b533cbb8aeb6fa720419aae381ac9784ad85d669
Jmacro <jonmacrohon@gmail.com>
2019-10-12 22:25:35 +1100

    add featured card and buttons that change carousel slide to portfolio.html (javascript + custom.js)

---
+++ assets/js/custom.js
@@ -0,0 +1,16 @@
+///////////////////// PORTFOLIO ///////////////////////
+
+//////// CAROUSEL CONTROLS //////////
+
+// Activate Carousel
+$("#carouselExampleControls").carousel();
+
+// Enable Carousel Indicators
+$("#carousel-button-1").click(function(){
+  $("#carouselExampleControls").carousel(0);
+});
+
+$("#carousel-button-2").click(function(){
+    $("#carouselExampleControls").carousel(1);
+  });
+

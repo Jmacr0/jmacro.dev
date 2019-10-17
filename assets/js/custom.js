f920c1b758540f5ca9d2b517f236777ed50f98ae
Jmacro <jonmacrohon@gmail.com>
2019-10-17 22:36:08 +1100

    improve responsiveness of portfolio. javascript active links working. add background to contact page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,25 @@
 ///////////////////// NAVBAR ///////////////////////
 
-$("li a").on("click", function(){
-  $("li a").find(".active").removeClass("active");
-  $(this).addClass("active");
+$(document).ready(function() {
+  $('a[href="/' + this.location.pathname + '"]').parent().addClass('active');
 });
 
+
+///////////////////// PORTFOLIO ///////////////////////
+
+//////// CAROUSEL CONTROLS //////////
+
+// Activate Carousel
+$("#carouselExampleControls").carousel();
+
+// Enable Carousel Indicators
+$("#carousel-button-1").click(function(){
+  $("#carouselExampleControls").carousel(0);
+});
+
+$("#carousel-button-2").click(function(){
+  $("#carouselExampleControls").carousel(1);
+});
 
 
 
@@ -53,19 +68,5 @@ var myPieChart = new Chart(ctx, {
 });
 
 
-///////////////////// PORTFOLIO ///////////////////////
 
-//////// CAROUSEL CONTROLS //////////
-
-// Activate Carousel
-$("#carouselExampleControls").carousel();
-
-// Enable Carousel Indicators
-$("#carousel-button-1").click(function(){
-  $("#carouselExampleControls").carousel(0);
-});
-
-$("#carousel-button-2").click(function(){
-    $("#carouselExampleControls").carousel(1);
-  });
 

8ee41951eaf19051710221ce7318ba1bbc8d9c96
Jmacro <jonmacrohon@gmail.com>
2019-10-18 16:42:12 +1100

    forgot to save prev commit changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -25,47 +25,7 @@ $("#carousel-button-2").click(function(){
 
 ///////////////////// ABOUT ///////////////////////
 
-var ctx = document.getElementById("lineChart");
-Chart.defaults.scale.ticks.beginAtZero = true;
-/* Chart.defaults.global.defaultFontSize = 5; */
 
-// For a pie chart
-var myPieChart = new Chart(ctx, {
-  type: 'doughnut',
-  data: {
-    labels: ['HTML', 'CSS', 'Javascript','Locked'],
-    datasets: [
-      {
-        label: 'Topics',
-        backgroundColor: ['#ff9100', '#008cff','#ffe600', '#ddd'],
-        borderWidth: 0,
-        
-        data: [1, 1, 1, 21]
-      }
-    ]
-  },
-  
-  options: {
-    
-    title: {
-      display: true,
-      text: "Bootcamp Progress",
-      fontSize: 20
-    },
-    
-   responsive: true,
-    cutoutPercentage: 20,
-    animation: {
-      animateScale: true
-    },
-    maintainAspectRatio : false,
-    legend: {
-      display: false,
-      position: 'right',
-    }
-  }
-  
-});
 
 
 

97767943eaf00e27609b8e4028763807e746ad14
Jon Anthony Macrohon <jonmacrohon@gmail.com>
2022-08-03 02:39:40 +1000

    Move particles as separate component.

---
+++ src/Particles.jsx
@@ -0,0 +1,98 @@
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
+const ParticlesBG = () => {
+	const particlesInit = async (main) => {
+		console.log(main);
+
+		// you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+		// this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+		// starting from v2 you can add only the features you need reducing the bundle size
+		await loadFull(main);
+	};
+
+	const particlesLoaded = (container) => {
+		console.log(container);
+	};
+	return (
+		<Particles
+			id="tsparticles"
+			init={particlesInit}
+			loaded={particlesLoaded}
+			options={{
+				background: {
+					color: {
+						value: "#4a8bed",
+					},
+				},
+				fpsLimit: 120,
+				interactivity: {
+					events: {
+						onClick: {
+							enable: false,
+							mode: "push",
+						},
+						onHover: {
+							enable: true,
+							mode: "repulse",
+						},
+						resize: true,
+					},
+					modes: {
+						push: {
+							quantity: 4,
+						},
+						repulse: {
+							distance: 100,
+							duration: 0.4,
+						},
+					},
+				},
+				particles: {
+					color: {
+						value: "#ffffff",
+					},
+					links: {
+						color: "#ffffff",
+						distance: 150,
+						enable: true,
+						opacity: 0.5,
+						width: 1,
+					},
+					collisions: {
+						enable: false,
+					},
+					move: {
+						direction: "none",
+						enable: true,
+						outModes: {
+							default: "bounce",
+						},
+						random: false,
+						speed: 3,
+						straight: false,
+					},
+					number: {
+						density: {
+							enable: true,
+							area: 800,
+						},
+						value: 100,
+					},
+					opacity: {
+						value: 0.5,
+					},
+					shape: {
+						type: "circle",
+					},
+					size: {
+						value: { min: 1, max: 5 },
+					},
+				},
+				detectRetina: true,
+			}}
+		/>
+	);
+};
+
+export default ParticlesBG;
